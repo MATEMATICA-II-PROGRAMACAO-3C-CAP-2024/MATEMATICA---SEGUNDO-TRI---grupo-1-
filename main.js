7ecb45b76611b8db0332d0ae757461004219d2e6
brendka <161617088+brendka@users.noreply.github.com>
2024-09-03 09:28:08 -0300

    grup 1

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 //codigo omitido 
 const numeroSenha = document.querySelector(".parametro-senha__texto");
+let tamanhoSenha = 12;
 const campoSenha = document.querySelector("#campo-senha");
 const letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVXYWZ";
 const letrasMinusculas = "abcdefghijklmnopqrstuvxywz";
 const numeros = "0123456789";
 const simbolos = "!@%*?";
 const botoes = document.querySelectorAll(".parametro-senha__botao");
+const botoes = document.querySelector( '#campo-senha')
 const checkbox = document.querySelectorAll(".checkbox");
 const forcaSenha = document.querySelector(".forca");
-const valorEntropia = document.querySelector(".entropia");
 
-let tamanhoSenha = 12;
+
 
 function geraSenha() {
-    let alfabeto = "";
+    let alfabeto = '';
     if (checkbox[0].checked) {
         alfabeto += letrasMaiusculas;
     }
@@ -27,7 +28,7 @@ function geraSenha() {
         alfabeto += simbolos;
     }
 
-    let senha = "";
+    let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
@@ -38,7 +39,7 @@ function geraSenha() {
     
     const entropia = tamanhoSenha * Math.log2(alfabeto.length);
     valorEntropia.textContent = "Um computador pode levar até " +
-        Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) + " dias para descobrir essa senha;";
+        Math.floor(numeroAleatorio);
     
     
     classificaSenha(entropia);
@@ -60,16 +61,18 @@ function aumentaTamanho() {
     geraSenha();
 }
 
-function classificaSenha(entropia) {
-    forcaSenha.classList.remove("fraca", "media", "forte");
+function classificaSenha(tamanhoAlfabeto) {
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto)
+    console.log(entropia)
+    forcaSenha.classList.remove('fraca', 'media', 'forte');
     if (entropia > 57) {
-        forcaSenha.classList.add("forte");
-    } else if (entropia > 35) {
-        forcaSenha.classList.add("media");
-    } else {
-        forcaSenha.classList.add("fraca");
+        forcaSenha.classList.add('forte');
+    } else if (entropia > 35 && entropia < 57 ) {
+        forcaSenha.classList.add('media');
+    } else if 
+        forcaSenha.classList.add('fraca');
     }
-}
+
 
 
 for (let i = 0; i < checkbox.length; i++) {
